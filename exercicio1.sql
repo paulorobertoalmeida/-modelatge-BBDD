7555fa948fe522eb9fadcaf736c791f22b0c4131
Paulo Almeida <62543673+paulorobertoalmeida@users.noreply.github.com>
2020-09-17 10:13:18 +0200

    Update ejercicio1.sql

--- exercicio1.sql
+++ exercicio1.sql
@@ -1,6 +1,12 @@
+-- MySQL Workbench Forward Engineering
+
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
 
 -- -----------------------------------------------------
 -- Schema mydb
@@ -18,6 +24,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Picture` (
   `Estado` MEDIUMTEXT NULL,
   PRIMARY KEY (`idPicture`))
 ENGINE = InnoDB;
+
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Vendedor`
@@ -64,6 +71,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Seats` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
